53ccde837064c336a4d0b7e0120b62d4585d15d5
IT21223808 <IT21223808@my.sliit.lk>
2025-11-26 18:18:53 +0530

    VehicleView font size updated

--- src/components/dashboard/Vehicle/VehicleView.tsx
+++ src/components/dashboard/Vehicle/VehicleView.tsx
@@ -275,7 +275,7 @@ const VehicleManagement = () => {
                     <div className="mb-6 overflow-x-auto rounded-lg border border-gray-200" style={{ scrollbarWidth: "thin" }}>
                         <table className="min-w-full bg-white">
                             <thead>
-                                <tr className="bg-gray-50 text-[#382A59] font-semibold text-[13px] sm:text-[14px] md:text-[15px] text-center font-poppins">
+                                <tr className="bg-gray-50 text-[#382A59] font-semibold text-[14px] sm:text-[15px] md:text-[16px] text-center font-poppins">
                                     <th className="px-3 py-3 whitespace-nowrap">Vehicle Id</th>
                                     <th className="px-3 py-3 whitespace-nowrap">V_Name</th>
                                     <th className="px-3 py-3 whitespace-nowrap">V_Type</th>
@@ -289,7 +289,7 @@ const VehicleManagement = () => {
 
                             <tbody className="font-poppins">
                                 {currentVehicles.map((v) => (
-                                    <tr key={v.id} className="border-b border-gray-100 text-center text-[12px] sm:text-[13px] md:text-[14px] hover:bg-gray-50">
+                                    <tr key={v.id} className="border-b border-gray-100 text-center text-[13px] sm:text-[14px] md:text-[15px] hover:bg-gray-50">
                                         <td className="py-3 px-2 text-gray-600 whitespace-nowrap">{v.id}</td>
                                         <td className="py-3 px-2 text-gray-600 whitespace-nowrap ">{v.name}</td>
                                         <td className="px-2 py-3 text-gray-600 whitespace-nowrap">{v.type}</td>
