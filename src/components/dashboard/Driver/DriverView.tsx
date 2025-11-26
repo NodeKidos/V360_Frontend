e534c16273e28ed4ecb750cbf06807ed650035d2
IT21223808 <IT21223808@my.sliit.lk>
2025-11-26 18:08:33 +0530

    DriverView font size updated

--- src/components/dashboard/Driver/DriverView.tsx
+++ src/components/dashboard/Driver/DriverView.tsx
@@ -301,7 +301,7 @@ const DriverManagement = () => {
                     <div className="mb-6 overflow-x-auto rounded-lg border border-gray-200" style={{ scrollbarWidth: "thin" }}>
                         <table className="min-w-full bg-white">
                             <thead>
-                                <tr className="bg-gray-50 text-[#382A59] font-semibold text-[13px] sm:text-[14px] md:text-[15px] text-center font-poppins">
+                                <tr className="bg-gray-50 text-[#382A59] font-semibold text-[14px] sm:text-[15px] md:text-[16px] text-center font-poppins">
                                     <th className="px-4 py-4 whitespace-nowrap">Driver Id</th>
                                     <th className="px-4 py-4 whitespace-nowrap">Driver Name</th>
                                     <th className="px-4 py-4 whitespace-nowrap">Contact No</th>
@@ -317,7 +317,7 @@ const DriverManagement = () => {
 
                             <tbody className="font-poppins">
                                 {currentDrivers.map((d) => (
-                                    <tr key={d.id} className="border-b border-gray-100 text-center text-[16px] sm:text-[16px] md:text-[16px] hover:bg-gray-50">
+                                    <tr key={d.id} className="border-b border-gray-100 text-center text-[13px] sm:text-[14px] md:text-[15px] hover:bg-gray-50">
                                         <td className="py-3 px-2" onClick={() => handleViewClick(d.id)}>
                                             <span className="text-blue-500 cursor-pointer">{d.id}</span> {/* Make ID clickable */}
                                         </td>                                       
