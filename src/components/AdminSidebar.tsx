c6c10ab3cca644e29598a09bd0e3622892d6de6c
IT21273858 <it21273858@my.sliit.lk>
2026-03-18 00:13:23 +0530

    unnecesary variables removed

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -7,7 +7,7 @@ import { TiThLargeOutline } from 'react-icons/ti';
 import { FaUser, FaHotel, FaCarSide, FaTruck, FaUsers, FaGift, FaRoute, FaClipboardList, FaMapMarkedAlt } from 'react-icons/fa';
 import { BiTrip } from 'react-icons/bi';
 import { FiLogOut } from 'react-icons/fi';
-import { IoGameController, IoPricetagOutline } from 'react-icons/io5';
+import { IoGameController } from 'react-icons/io5';
 import logo from '../assets/favicon.png'; // Your logo
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { IoMdClose, IoMdSettings } from 'react-icons/io';
